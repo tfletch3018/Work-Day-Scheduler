d638abe569203f6ca5396c7e573b5cd3be894986
Tonya Fletcher <tfletch4504@gmail.com>
2021-03-13 14:23:43 -0500

    define variables

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+var currentDay = moment().format('dddd') + " " + moment().format("D MMMM YYYY");
+var currentTime = moment().format('hh:mm:ss a');
+var hourNine = $("#9am");
+var hourTen = $("#10am");
+var hourEleven = $("#11am");
+var hourTwelve = $("#12pm");
+var hourThirteen = $("#13pm");
+var hourFourteen = $("#14pm");
+var hourFifteen = $("#15pm");
+var hourSixteen = $("#16pm");
+var hourSeventeen = $("#17pm");
+var hourEighteen = $("#18pm");
+var hourNineteen = $("#19pm");
+
+var hour = moment().hour();
+var userInside;
+var hourSpan;
+
+var interval = setInterval(function() {
+    var timeNow = moment();
+    $('#currentDay').html(timeNow.format('YYYY MMMM DD') + ' '
+    + timeNow.format('dddd')
+    .substring(0, 3).toUpperCase());
+    $('#currentDay').html(currentDay + " " + timeNow.format('hh:mm:ss A'));
+}, 100);
+
+function initPage() {
+    
+}
